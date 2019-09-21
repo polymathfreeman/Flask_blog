94ebdacba01ffaf33337b68feee9c466c3ae8432
Polymath Freeman <polymathfreeman@gmail.com>
2019-09-21 20:14:23 +0200

    Part2 finished

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Polymath Freeman',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 21, 2019'
+    },
+    {
+        'author': 'Emacs Freeman',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 21, 2019'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
